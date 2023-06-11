4d319540c7b9d0093325346710dcd142a0cddb18
SephVII <avalancheseph@yahoo.com>
2023-06-11 17:18:51 +0200

    2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.addEventListener("DOMContentLoaded", function () {
-    var dropdowns = document.getElementsByClassName("dropdown");
-    for (var i = 0; i < dropdowns.length; i++) {
-      dropdowns[i].addEventListener("mouseover", function () {
-        this.children[0].style.display = "block";
+    var dropdownParents = document.querySelectorAll("nav ul li");
+    for (var i = 0; i < dropdownParents.length; i++) {
+      dropdownParents[i].addEventListener("mouseover", function () {
+        this.querySelector("ul.dropdown").style.display = "block";
       });
-      dropdowns[i].addEventListener("mouseout", function () {
-        this.children[0].style.display = "none";
+      dropdownParents[i].addEventListener("mouseout", function () {
+        this.querySelector("ul.dropdown").style.display = "none";
       });
     }
   });
+  
   
