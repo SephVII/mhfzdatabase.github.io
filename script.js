e55b4fec676686d0f153339bb5f1a8adfba553e2
SephVII <avalancheseph@yahoo.com>
2023-06-11 17:35:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 window.addEventListener("DOMContentLoaded", function () {
-    var dropdowns = document.getElementsByClassName("dropdown");
-    for (var i = 0; i < dropdowns.length; i++) {
-      var dropdown = dropdowns[i];
-      var subcategories = dropdown.querySelectorAll("li");
-      for (var j = 0; j < subcategories.length; j++) {
-        subcategories[j].style.display = "none";
-      }
-  
-      dropdown.parentNode.addEventListener("mouseover", function () {
-        var subcategories = this.querySelector("ul.dropdown").querySelectorAll("li");
-        for (var j = 0; j < subcategories.length; j++) {
-          subcategories[j].style.display = "block";
+    var dropdownParents = document.querySelectorAll("nav ul li");
+    for (var i = 0; i < dropdownParents.length; i++) {
+      dropdownParents[i].addEventListener("mouseover", function () {
+        var dropdown = this.querySelector("ul.dropdown");
+        if (dropdown) {
+          dropdown.style.display = "block";
         }
       });
-      dropdown.parentNode.addEventListener("mouseout", function () {
-        var subcategories = this.querySelector("ul.dropdown").querySelectorAll("li");
-        for (var j = 0; j < subcategories.length; j++) {
-          subcategories[j].style.display = "none";
+      dropdownParents[i].addEventListener("mouseout", function () {
+        var dropdown = this.querySelector("ul.dropdown");
+        if (dropdown) {
+          dropdown.style.display = "none";
         }
       });
     }
   });
-  
   
