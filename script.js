a7a32e2a474a3d0243e5feb16686f75d30613944
SephVII <avalancheseph@yahoo.com>
2023-06-11 17:39:11 +0200

    22

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ window.addEventListener("DOMContentLoaded", function () {
       dropdownParents[i].addEventListener("mouseover", function () {
         var dropdown = this.querySelector("ul.dropdown");
         if (dropdown) {
+          closeAllDropdowns();
           dropdown.style.display = "block";
         }
       });
-      dropdownParents[i].addEventListener("mouseout", function () {
-        var dropdown = this.querySelector("ul.dropdown");
-        if (dropdown) {
-          dropdown.style.display = "none";
-        }
-      });
+    }
+  
+    function closeAllDropdowns() {
+      var dropdowns = document.querySelectorAll("nav ul ul.dropdown");
+      for (var i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].style.display = "none";
+      }
     }
   });
   
